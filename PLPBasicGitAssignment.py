df2c90332d0158fa1bc90d61dba2dff2a8da19f2
NennyTol <159932482+NennyTol@users.noreply.github.com>
2024-05-22 22:30:48 +0100

    Add files via upload

---
+++ PLPBasicGitAssignment.py
@@ -0,0 +1,6 @@
+# Open the file in write mode ('w')
+with open('hello.txt', 'w') as file:
+    # Write the text "Hello, world!" into the file
+    file.write('Hello, world!\n')
+
+print("File 'hello.txt' created successfully!")
